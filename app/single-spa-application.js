f9d25082cf9c21c4497b7974e94533dfbd6e74d2
Joel Denning <joeldenning@gmail.com>
2021-03-29 12:24:29 -0600

    Self review

--- app/single-spa-application.js
+++ app/single-spa-application.js
@@ -1,11 +1,11 @@
 import singleSpaAngularJS from 'single-spa-angularjs';
-import angular from 'angular';
+import 'angular';
 // import "systemjs-webpack-interop/auto-public-path";
 
 console.log('singleSpaAngularJS', singleSpaAngularJS)
 
 const ngLifecycles = singleSpaAngularJS({
-  angular: angular,
+  angular: window.angular,
   mainAngularModule: 'myApp',
   uiRouter: true,
   preserveGlobal: false,
